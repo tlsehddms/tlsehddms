7fd40c5b299b2af7c78ae3a1763940b83b33496b
tlsehddms <douko@naver.com>
2025-04-16 16:20:01 +0900

    Update map-of-korea.py

--- map-of-korea.py
+++ map-of-korea.py
@@ -1,12 +1,10 @@
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
 
-# 지도 유형 목록
+# 저작권 걱정 없는 한반도 백지도를 위한 이미지 URL
 map_types = {
-    "행정구역": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/South_Korea_administrative_divisions_map.png/800px-South_Korea_administrative_divisions_map.png",
-    "지형도": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/South_Korea_topographic_map_2.png/800px-South_Korea_topographic_map_2.png",
-    "위성지도": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/South_Korea_satellite_map.png/800px-South_Korea_satellite_map.png",
+    "행정구역": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/South_Korea_administrative_divisions_identity_map.svg/2000px-South_Korea_administrative_divisions_identity_map.svg.png",
+    "지형도": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Topographic_Map_of_Korea.svg/2000px-Topographic_Map_of_Korea.svg.png",
+    "위성지도": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/South_Korea_satellite_photo.png/800px-South_Korea_satellite_photo.png",
 }
 
 # 스트림릿 제목
